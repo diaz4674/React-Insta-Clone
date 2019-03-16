0faa843518f02956ad0a03c090a34d38bb65882d
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-03-16 14:02:27 -0700

    fixed Search Bar functionality

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -27,13 +27,6 @@ class App extends Component {
       search: event.target.value
     })
     
-    if(this.state.search.length > 0){
-      const filteredData = this.state.data.filter((word) => {
-        return word.username.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;})
-        this.setState({data: filteredData})
-    } else {
-      this.setState({data: dummyData})
-    }
   }
 
 
@@ -42,7 +35,8 @@ class App extends Component {
     return (
       <div className="App">
         <SearchBar searchHandler ={this.searchHandler} search ={this.statesearch} />
-       { this.state.data.map(post => {
+       { this.state.data.filter((word) => {
+        return word.username.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;}).map(post => {
          return(
            <div>
             <PostContainer timestamp = {post.timestamp} thumbnail = {post.thumbnailUrl} username = {post.username} img = {post.imageUrl} likes = {post.likes} comments = {post.comments} newComment ={this.newComment} addComentHandler ={this.addComentHandler} changeHandler = {this.changeHandler} comment = {this.state.comment} newComment = {this.state.newComment}/>
